859be4d126cd168a28fd1075965a0ffde3b3f0f2
Nathan Flint <nlflint@gmail.com>
2014-01-14 10:32:11 -0800

    Fixed circle so that its based on the left and right side of the flag. Removed doubleBaseUnitOfMeasure and scaled uses of it so baseUnitOfMeasure could be used instead.

--- Flags/src/Flags.java
+++ Flags/src/Flags.java
@@ -13,38 +13,37 @@ public class Flags
     {
         NsccWindow mainWindow = new NsccWindow(100,100,400,350);
 
-        int baseUnitOfMeasure = 22;
-        int doubleBaseUnitOfMeasure = baseUnitOfMeasure * 2;
-        int flagLeftBoundary = 30;
-        int flagTopBoundary = 30;
+        //This is the base unit of the whole flag. Changing it will scale the entire flag up or down.
+        int baseUnitOfMeasure = 12;
+        int flagLeftBoundary = 10;
+        int flagTopBoundary = 10;
 
-        int flagHoist = 6 * doubleBaseUnitOfMeasure;
-        int flagFly = 7 * doubleBaseUnitOfMeasure;
+        int flagHoist = 24 * baseUnitOfMeasure;
+        int flagFly = 28 * baseUnitOfMeasure;
         NsccRectangle baseLayer = new NsccRectangle(flagLeftBoundary,flagTopBoundary,flagFly,flagHoist);
         baseLayer.setBackground(Color.ORANGE);
         baseLayer.setFilled(true);
         mainWindow.add(baseLayer);
 
         int middleStripeX = flagLeftBoundary;
-        int middleStripeY = flagTopBoundary + (2 * doubleBaseUnitOfMeasure);
-        int middleStripHeight = 2 * doubleBaseUnitOfMeasure + 1;
+        int middleStripeY = flagTopBoundary + (8 * baseUnitOfMeasure);
+        int middleStripHeight = 8 * baseUnitOfMeasure + 1;
         NsccRectangle middleStripe = new NsccRectangle(middleStripeX,middleStripeY,flagFly,middleStripHeight);
         middleStripe.setBackground(Color.WHITE);
         middleStripe.setFilled(true);
         mainWindow.add(middleStripe);
 
         int bottomStripeX = flagLeftBoundary;
-        int bottomStripeY = flagTopBoundary + (4 * doubleBaseUnitOfMeasure);
-        int bottomStripHeight = 2 * doubleBaseUnitOfMeasure;
+        int bottomStripeY = flagTopBoundary + (16 * baseUnitOfMeasure);
+        int bottomStripHeight = 8 * baseUnitOfMeasure;
         NsccRectangle bottomStripe = new NsccRectangle(bottomStripeX,bottomStripeY,flagFly,bottomStripHeight);
         bottomStripe.setBackground(Color.GREEN);
         bottomStripe.setFilled(true);
         mainWindow.add(bottomStripe);
 
-        int verticalBorderWidthOfMiddleStripe = 2;
-        int middleCircleX = 7 * baseUnitOfMeasure;
-        int middleCircleY = 6 * baseUnitOfMeasure - verticalBorderWidthOfMiddleStripe;
-        int circileWidth = 3 * baseUnitOfMeasure;
+        int middleCircleX = flagLeftBoundary + (11 * baseUnitOfMeasure);
+        int middleCircleY = flagTopBoundary + (9 * baseUnitOfMeasure);
+        int circileWidth = 6 * baseUnitOfMeasure;
         NsccEllipse middleCircle = new NsccEllipse(middleCircleX, middleCircleY,circileWidth,circileWidth);
         middleCircle.setBackground(Color.ORANGE);
         middleCircle.setFilled(true);
