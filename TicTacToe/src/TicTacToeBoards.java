8f241c4af41c608d4f2e0e9d266d4287d23c636c
Nathan Flint <nlflint@gmail.com>
2014-01-25 19:27:33 -0800

    Fixed up documentation

--- TicTacToe/src/TicTacToeBoards.java
+++ TicTacToe/src/TicTacToeBoards.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 
 /**
- * This class draws several different tic tac toe boards to a window
+ * This class draws several different tic tac toe boards to a window.
  *
  * @author Nathan Flint
  * @version Assignment 3: Tic-Tac-Toe
@@ -37,13 +37,13 @@ public class TicTacToeBoards
         // Draws a regular tic tac toe board
         drawTicTacToeBoard(40, 40);
 
-        // Draws a board with line thickness
+        // Draws a board with thick lines
         drawTicTacToeBoard(40, 170, 6);
 
         // Draws a board colored red
         drawTicTacToeBoard(170, 40, Color.RED);
 
-        // Draws a board rotated pi/8 radians
+        // Draws a board rotated pi/8 radians around its center
         drawTicTacToeBoard(170, 170, Math.PI/8);
     }
 
@@ -143,12 +143,12 @@ public class TicTacToeBoards
 
     /**
      * Draws a black rotated tic tac toe board. Arguments supplied will indicate the (x, y) coordinates
-     * of the upper-left corner of the bounding box for the tic-tac-toe board, and the rotation in radians around
-     * the center of the board.
+     * of the upper-left corner of the bounding box for the tic-tac-toe board before rotation, and the
+     * rotation in radians that the board will be rotation around its center.
      *
-     * @param originX X component of the upper left corner of the board.
-     * @param originY Y component of the upper left corner of the board.
-     * @param radiansRotation Radians to rotate the board.
+     * @param originX X component of the upper left corner of the board before rotation.
+     * @param originY Y component of the upper left corner of the board before rotation.
+     * @param radiansRotation Radians to rotate around the center of the board.
      */
     public void drawTicTacToeBoard(int originX, int originY, double radiansRotation)
     {
@@ -157,62 +157,61 @@ public class TicTacToeBoards
     }
 
     // This private method allows setting location, color and radians rotation at the same time.
-    public void drawTicTacToeBoard(int originX, int originY, Color lineColor, double radiansRotation)
+    public void drawTicTacToeBoard(int originX, int originY, Color lineColor, double radiansOfRotation)
     {
         /*
-         Defines side length of the boxes that make the tic tac toe board. There are 9
-         of these boxes in a tic tac toe board. Changing this will alter the overall
-         size of the entire board. Standard value is 30.
+         Defines side length of the boxes that make the board. There are 9
+         of these boxes in a tic tac toe board. Changing this will alter the size of
+         the entire board. Standard value is 30.
           */
-        //
-        int boxUnitWidth = 30;
+        int boxWidth = 30;
 
         // This point is the middle of the board. It will be used for rotating.
-        Point boardMidpoint = new Point(
-                originX + boxUnitWidth * 3 / 2,
-                originY + boxUnitWidth * 3 / 2);
+        Point boardCenterPoint = new Point(
+                originX + boxWidth * 3 / 2,
+                originY + boxWidth * 3 / 2);
 
         // Draws the left vertical line of a board based on the given origin coordinates.
         NsccLine leftVerticleLine = CreateRotatedLine(
-                originX + boxUnitWidth,
+                originX + boxWidth,
                 originY,
-                originX + boxUnitWidth,
-                originY + boxUnitWidth * 3,
-                radiansRotation,
-                boardMidpoint);
+                originX + boxWidth,
+                originY + boxWidth * 3,
+                radiansOfRotation,
+                boardCenterPoint);
         leftVerticleLine.setForeground(lineColor);
         mainWindow.add(leftVerticleLine);
 
         // Draws the right vertical line of a board based on the given origin coordinates.
         NsccLine rightVerticleLine = CreateRotatedLine(
-                originX + boxUnitWidth * 2,
+                originX + boxWidth * 2,
                 originY,
-                originX + boxUnitWidth * 2,
-                originY + boxUnitWidth * 3,
-                radiansRotation,
-                boardMidpoint);
+                originX + boxWidth * 2,
+                originY + boxWidth * 3,
+                radiansOfRotation,
+                boardCenterPoint);
         rightVerticleLine.setForeground(lineColor);
         mainWindow.add(rightVerticleLine);
 
         // Draws the top  horizontal line of a board based on the given origin coordinates.
         NsccLine topHorizontalLine = CreateRotatedLine(
                 originX,
-                originY + boxUnitWidth,
-                originX + boxUnitWidth * 3,
-                originY + boxUnitWidth,
-                radiansRotation,
-                boardMidpoint);
+                originY + boxWidth,
+                originX + boxWidth * 3,
+                originY + boxWidth,
+                radiansOfRotation,
+                boardCenterPoint);
         topHorizontalLine.setForeground(lineColor);
         mainWindow.add(topHorizontalLine);
 
         // Draws the top  horizontal line of a board based on the given origin coordinates.
         NsccLine bottomHorizontalLine = CreateRotatedLine(
                 originX,
-                originY + boxUnitWidth * 2,
-                originX + boxUnitWidth * 3,
-                originY + boxUnitWidth * 2,
-                radiansRotation,
-                boardMidpoint);
+                originY + boxWidth * 2,
+                originX + boxWidth * 3,
+                originY + boxWidth * 2,
+                radiansOfRotation,
+                boardCenterPoint);
         bottomHorizontalLine.setForeground(lineColor);
         mainWindow.add(bottomHorizontalLine);
 
@@ -222,33 +221,33 @@ public class TicTacToeBoards
 
     /*
     This method will create a line with the specified
-    end points, that is rotated around the midpoint
-    by the specified radians.
+    end points, that is rotated around the specified
+    center of rotation by the specified radians.
      */
-    private NsccLine CreateRotatedLine(int x1, int y1, int x2, int y2, double radiansRotation, Point rotationPoint)
+    private NsccLine CreateRotatedLine(int x1, int y1, int x2, int y2, double radians, Point centerOfRotation)
     {
-        // Creates the first end point of line
-        Point firstRotatedPoint = RotatePoint(x1,y1,radiansRotation, rotationPoint);
+        // Creates the first end point of the line
+        Point firstRotatedPoint = RotatePoint(x1, y1, radians, centerOfRotation);
 
         // Creates th second end point of the line
-        Point secondRotatedPoint = RotatePoint(x2,y2,radiansRotation, rotationPoint);
+        Point secondRotatedPoint = RotatePoint(x2, y2, radians, centerOfRotation);
 
         // Returns a new rotated line
         return new NsccLine(
-                (int)firstRotatedPoint.getX(),
-                (int)firstRotatedPoint.getY(),
-                (int)secondRotatedPoint.getX(),
-                (int)secondRotatedPoint.getY());
+                (int) firstRotatedPoint.getX(),
+                (int) firstRotatedPoint.getY(),
+                (int) secondRotatedPoint.getX(),
+                (int) secondRotatedPoint.getY());
     }
 
     // Rotates a point around a given rotation point by specified radians.
-    private Point RotatePoint(int x, int y, double radians, Point rotationPoint)
+    private Point RotatePoint(int x, int y, double radians, Point centerOfRotation)
     {
-        // X component of the rotation point
-        double a = rotationPoint.getX();
+        // X component of the center of rotation
+        double a = centerOfRotation.getX();
 
-        // Y component of the rotation point
-        double b = rotationPoint.getY();
+        // Y component of the center of rotation
+        double b = centerOfRotation.getY();
 
         // Distance from given x,y to rotation-point. This will be radius used for rotation.
         double radius = Math.sqrt(
@@ -256,17 +255,17 @@ public class TicTacToeBoards
                 ((y - b) * (y - b)));
 
         // Converts rectangular coordinates to polar coordinates
-        double omega = Math.atan2(y - rotationPoint.getY(), x - rotationPoint.getX());
+        double omega = Math.atan2(y - b, x - a);
 
         // Rotates the x coordinate
         double rotatedX = radius *
                 (Math.cos(radians) * Math.cos(omega) - Math.sin(radians) * Math.sin(omega))
-                + rotationPoint.getX();
+                + a;
 
         // Rotates the y coordinate
         double rotatedY = radius *
                 (Math.sin(radians) * Math.cos(omega) + Math.cos(radians) * Math.sin(omega))
-                + rotationPoint.getY();
+                + b;
 
         // Returns the rotated point
         return new Point((int)rotatedX, (int)rotatedY);
