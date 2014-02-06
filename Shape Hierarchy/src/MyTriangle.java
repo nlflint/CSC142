116ec1c1fb572fe0364fe42a0ab3128c118d315b
Nathan Flint <nlflint@gmail.com>
2014-02-06 12:48:03 -0800

    Added MyTriangle class

---
+++ Shape Hierarchy/src/MyTriangle.java
@@ -0,0 +1,23 @@
+/**
+ * This class represents a triangle and
+ * inherits from MyShape.
+ *
+ * @author Nathan Flint
+ * @version Assignment 5: Shape Hierarchy
+ *
+ * Grading level: Challenge
+ */
+public class MyTriangle extends MyShape {
+
+    /**
+     * Constructor for MyTriangle.
+     *
+     * @param base
+     * @param height
+     */
+    public MyTriangle(int base, int height)
+    {
+        // Call the MyShape constructor
+        super(base,height);
+    }
+}
