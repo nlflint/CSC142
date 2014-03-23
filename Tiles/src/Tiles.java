5c9cac9508336786f41631f26b9faee5760e1e51
Nathan Flint <nlflint@gmail.com>
2014-03-22 20:41:28 -0700

    Tilling: Implemented Med1 pattern

--- Tiles/src/Tiles.java
+++ Tiles/src/Tiles.java
@@ -94,7 +94,78 @@ public class Tiles
 
     public void drawMed1()
     {
+        // Create drawing panel
+        DrawingPanel panel = new DrawingPanel(WIDTH,HEIGHT);
+        panel.setTitle("BasketWeave");
 
+
+        // Get drawing surface
+        Graphics drawingSurface = panel.getGraphics();
+
+        // define tile width and height
+        final int tileWidth = 24;
+        final int tileHeight = 24;
+
+        // identify how many tiles will be needed
+        int tileCountAcross = WIDTH / tileWidth + 1;
+        int tileCountDown = HEIGHT / tileHeight + 1;
+        int totalTiles = tileCountAcross * tileCountDown;
+
+        // we know how many tiles to draw, so loop through the total.
+        for (int tileIndex = 0; tileIndex < totalTiles; tileIndex++)
+        {
+            // Get column and row of current tiles index
+            int currentColumn = tileIndex % tileCountAcross;
+            int currentRow = tileIndex / tileCountAcross;
+
+            // Calculates x and y coordinate of next drawing tiles
+            int x = currentColumn * tileWidth;
+            int y = currentRow * tileHeight;
+
+            // Draws the tile
+            drawMed1Tile(drawingSurface, x, y, tileWidth, tileHeight);
+        }
+
+    }
+
+    // draws a med 1 tile with given dimensions
+    private void drawMed1Tile(Graphics g, int x, int y, int width, int height)
+    {
+        // Fill in red background
+        g.setColor(Color.red);
+        g.fillRect(x, y, width, height);
+
+        // Setup points to draw an octagon
+        int diamondRadius = (int) (width * (7 / 24.0));
+        int[] xPoints = new int[] {
+                x + diamondRadius,
+                x + diamondRadius + 10,
+                x + width,
+                x + width,
+                x + diamondRadius + 10,
+                x + diamondRadius,
+                x,
+                x
+        };
+
+        int[] yPoints = new int[] {
+                y,
+                y,
+                y + diamondRadius,
+                y + diamondRadius + 10,
+                y + height,
+                y + height,
+                y + diamondRadius + 10,
+                y + diamondRadius
+        };
+
+        // draw octagon
+        g.setColor(new Color(255,255,64));
+        g.fillPolygon(xPoints, yPoints, xPoints.length);
+
+        // draw lines
+        g.setColor(Color.BLACK);
+        g.drawPolygon(xPoints, yPoints, xPoints.length);
 
     }
 
@@ -102,5 +173,6 @@ public class Tiles
     {
         Tiles tiles = new Tiles();
         tiles.drawBasketWeave();
+        tiles.drawMed1();
     }
 }
